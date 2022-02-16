70a884adcc9ed591843a0c00b57c49893170b5f9
viswa <kasiviswa@gmail.com>
2022-02-16 10:07:58 +0530

    adding the base/first file

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whatsapp
+{
+    class Class1
+    {
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("click 1 to add a conract");
+        Console.WriteLine("click 2 to edit a contact");
+        Console.WriteLine("click 33 to type a message");
+        Console.WriteLine("click 4 to see the clients");
+        int x = Console.Read();
+
+
+
+        List<contacts> k = new List<contacts>();
+        k.Add(new contacts());
+        k[0].phone = Console.Read();
+
+
+    }
+}
